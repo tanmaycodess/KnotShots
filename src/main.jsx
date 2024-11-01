e1cbba5c53eeb71709dac7f88e2f5633265ab6e5
unknown <sethtanmay72@gmail.com>
2024-11-01 11:15:50 +0530

    fourth

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import './index.css'
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './Pages/Home/home.jsx'
 import Contact from './Components/Contact/Contact.jsx'
 import PhotographyPage from './Components/Photography/Photography.jsx'
+import Home from './Pages/Home/Home.jsx'
 
 const router = createBrowserRouter([
     {
